202f03362f192a1e6ea4630280904a04ea9c888a
tr <tnreja@gmail.com>
2021-11-16 19:49:15 -0700

    oops more fixes too

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   Drive drive = new Drive();
-  int x = 3;
 
   private Command m_autonomousCommand;
 
